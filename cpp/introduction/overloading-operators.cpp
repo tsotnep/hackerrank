169938beede1f12e3c243b743d9e0a0f65686f4c
Tsotne Putkaradze <tsotnep@gmail.com>
2016-04-26 14:03:01 +0300

    overloading of + and << operators

---
+++ cpp/introduction/overloading-operators.cpp
@@ -0,0 +1,19 @@
+//Overload operators + and << for the class complex
+//+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
+//<< should print a complex number in the format "a+ib"
+
+Complex operator + (Complex x, Complex y) {
+    int sumA=x.a + y.a;
+    int sumB=x.b + y.b;
+    Complex ans;
+    string s=to_string(sumA)+'+'+'i'+to_string(sumB);
+    ans.input(s);
+    return ans;
+}
+
+ostream& operator<< (ostream& output, Complex x)
+{
+    output << to_string(x.a)+'+'+'i'+to_string(x.b) << endl;
+ return output;
+}
+
