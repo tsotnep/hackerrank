02049a8c6471befa7e8701ebbfab831e2d067368
Tsotne Putkaradze <tsotnep@gmail.com>
2016-04-26 22:20:23 +0300

    link added

--- cpp/introduction/overload-operators.cpp
+++ cpp/introduction/overload-operators.cpp
@@ -1,3 +1,5 @@
+//https://www.hackerrank.com/challenges/overload-operators
+
 //Overload operators + and << for the class complex
 //+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
 //<< should print a complex number in the format "a+ib"
@@ -16,4 +18,3 @@ ostream& operator<< (ostream& output, Complex x)
     output << to_string(x.a)+'+'+'i'+to_string(x.b) << endl;
  return output;
 }
-
