f33effa4ce42cd5afa39984182aef8e4aced19ee
Tsotne Putkaradze <tsotnep@gmail.com>
2016-04-27 15:12:27 +0300

    fails on two tests only, they are specified here

--- cpp/introduction/bit-array.cpp
+++ cpp/introduction/bit-array.cpp
@@ -1,33 +1,47 @@
+//https://www.hackerrank.com/challenges/bitset-1
 #include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 
 int main() {
-   /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-   unsigned long long n,s,p,q;
-   cin>>n>>s>>p>>q;
-   //n=3, s=p=q=1;
+   int d=0;
+   set<int>seeds;
+   unsigned long long n,s,p,q,
+   countedIterationsBefore=1, iterationsOnCurrentSeed=0,
+   modulus=0, nextVal=0, currVal=0;
 
-   long long a[n], k=1, i, mod=(1<<31);
-   mod-=k;
+   //test inputs
+   n=3; s=p=q=1;
+   n=10000000; s=658061970; p=695098531; q=1430548937;
+   n=100000000; s=569099406; p=1607140150; q=823906344; //should be 31, and is 100000000
+   // cin>>n>>s>>p>>q;
 
-   a[0] = s & mod;
+   modulus=(1<<31);
+   modulus--;
+   nextVal=s & modulus;
 
-   for (i=1;i<n;i++){
-      a[i]=(a[i-1]*p+q) & (mod);
+   while(countedIterationsBefore<n){
+      iterationsOnCurrentSeed=0;
+      while(
+         nextVal<modulus &&
+         countedIterationsBefore + iterationsOnCurrentSeed < n
+      )
+      {
+         currVal = nextVal;
+         iterationsOnCurrentSeed++;
+         nextVal=currVal*p+q;
+      }
+      currVal=nextVal & modulus;
+      if (seeds.find(currVal) == seeds.end()){
+         countedIterationsBefore+=iterationsOnCurrentSeed;
+         seeds.insert(currVal);
+      }
    }
-
-   //an es unda meqna an Set-shi udna chameyara tavidanve
-   sort(a, a+n);
-   for (i=1;i<n;i++){
-      if (a[i]!=a[i-1]) k++;
-   }
-
-   cout<<k<<endl;
-
+   cout<<countedIterationsBefore<<endl;
    return 0;
 }
