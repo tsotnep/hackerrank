cb9b77468f2e380eaaae5a2f4d520789ff9e9c97
Tsotne Putkaradze <tsotnep@gmail.com>
2016-07-03 17:00:38 +0400

    simple commit

--- algorithms/search/connected-cell-in-a-grid.cpp
+++ algorithms/search/connected-cell-in-a-grid.cpp
@@ -5,32 +5,31 @@
 #include <algorithm>
 using namespace std;
 
-vector< vector<int> > v(10, vector<int>(10));
+vector< vector<int> > v(10, vector<int>(10,0));
 
 int RECU(int i, int j){
-    // cout<<"ee";
-    if  (   j>=0 && j<10 &&
-            i>=0 && i<10
-        )
-    {
+    cout<<"ENTER: "<<i<<" "<<j<<endl;
+    if  (   j>=0 && j<10 && i>=0 && i<10   ){
+        cout<<"IF cond"<<endl;
 
-    // for (i=0;i<10;i++)
-    //     for (j=0;j<10;j++)
     //         cout<<v[i][j]<<" ";
     //is it possible that, if code below, will enter eternal loop, the cout line aboce, won't work.?
-        if (v[i][j-1]==1) return 1+RECU(i,j-1);   cout<<"IN0 " << i<<" "<<j<<endl;
-        if (v[i][j+1]==1) return 1+RECU(i,j+1);   cout<<"IN1 " << i<<" "<<j<<endl;
-        if (v[i-1][j]==1) return 1+RECU(i-1,j);   cout<<"IN2 " << i<<" "<<j<<endl;
-        if (v[i+1][j]==1) return 1+RECU(i+1,j);   cout<<"IN3 " << i<<" "<<j<<endl;
 
-        if (v[i-1][j-1]==1) return 1+RECU(i-1,j-1);   cout<<"IN4 " << i<<" "<<j<<endl;
-        if (v[i-1][j+1]==1) return 1+RECU(i-1,j+1);   cout<<"IN5 " << i<<" "<<j<<endl;
-        if (v[i+1][j-1]==1) return 1+RECU(i+1,j-1);   cout<<"IN6 " << i<<" "<<j<<endl;
-        if (v[i+1][j+1]==1) return 1+RECU(i+1,j+1);   cout<<"IN7 " << i<<" "<<j<<endl;
+        //up, down, left, right
+        if (v[i][j-1]   ==1)        {   v[i][j-1]   = 0;   cout<<"IN0 W " <<i<<" "<<j<<endl;   return 1+RECU(i,j-1);   }
+        if (v[i][j+1]   ==1)        {   v[i][j+1]   = 0;   cout<<"IN1 E " <<i<<" "<<j<<endl;   return 1+RECU(i,j+1);   }
+        if (v[i+1][j]   ==1)        {   v[i+1][j]   = 0;   cout<<"IN3 S " <<i<<" "<<j<<endl;   return 1+RECU(i+1,j);   }
+        if (v[i-1][j]   ==1)        {   v[i-1][j]   = 0;   cout<<"IN2 N " <<i<<" "<<j<<endl;   return 1+RECU(i-1,j);   }
+
+        //diagonals ;
+        if (v[i-1][j-1] ==1)        {   v[i-1][j-1] = 0;   cout<<"IN4 NW " <<i<<" "<<j<<endl;   return 1+RECU(i-1,j-1); }
+        if (v[i-1][j+1] ==1)        {   v[i-1][j+1] = 0;   cout<<"IN5 NE " <<i<<" "<<j<<endl;   return 1+RECU(i-1,j+1); }
+        if (v[i+1][j-1] ==1)        {   v[i+1][j-1] = 0;   cout<<"IN6 SW " <<i<<" "<<j<<endl;   return 1+RECU(i+1,j-1); }
+        if (v[i+1][j+1] ==1)        {   v[i+1][j+1] = 0;   cout<<"IN7 SE " <<i<<" "<<j<<endl;   return 1+RECU(i+1,j+1); }
         return 0;
     }
     else{
-        // cout<<"ELSEE";
+        cout<<"ELSE cond"<<endl;
         return 0;
     }
 }
@@ -39,7 +38,7 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int m,n,i,j;
     cin>>m>>n;
-    // vector< vector<int> > v(m, vector<int>(n));
+
     for (i=0;i<m;i++)
         for (j=0;j<n;j++){
             cin>>v[i][j];
@@ -48,17 +47,11 @@ int main() {
     int Max=-1, max=-1;
     for (i=0;i<m;i++)
         for (j=0;j<n;j++){
-            // cout << i<<" "<<j<<endl;
-            max=RECU(i,j);
+            cout<<"IN L "<<i<<" "<<j<<endl;
+            if (v[i][j]==1) {v[i][j]=0; max=1+RECU(i,j);}
             if (max>Max) Max=max;
         }
     cout <<Max;
-
-
-    // max=RECU(0,0);
-    // cout <<max;
-
-
 
     return 0;
 }
--- cpp/introduction/bit-array.cpp
+++ cpp/introduction/bit-array.cpp
@@ -12,36 +12,53 @@ int main() {
    int d=0;
    set<int>seeds;
    unsigned long long n,s,p,q,
-   countedIterationsBefore=1, iterationsOnCurrentSeed=0,
+   countedIterBefore=0, iterOnCurrentSeed=0,
    modulus=0, nextVal=0, currVal=0;
 
    //test inputs
    n=3; s=p=q=1;
-   n=10000000; s=658061970; p=695098531; q=1430548937;
-   n=100000000; s=569099406; p=1607140150; q=823906344; //should be 31, and is 100000000
+   n=10000000;  s=658061970;  p=695098531;  q=1430548937;
+   n=100000000; s=569099406;  p=1607140150; q=823906344;  //FAIL: should be 31, and is 100000000
+   n=100000000; s=1232077670; p=126810854;  q=1536183938; //FAIL: should be 26, and is 100000000
+   n=100000000; s=145466602330; p=126810854; q=1536183938; //should jump over modulus after second iteration
    // cin>>n>>s>>p>>q;
 
-   modulus=(1<<31);
-   modulus--;
-   nextVal=s & modulus;
+   // modulus=(1<<31);
+   // modulus--; //=18446744071562067967
+   modulus = 2147483648; //actual 2^31 - 1
+   nextVal=s % modulus;
+// cout << "s "<< s <<endl;
+// cout << "modulus "<< modulus<<endl;
+// cout << "nextVal "<< nextVal<<endl;
+// cout << "nextVal+modulus "<< nextVal+modulus<<endl;
 
-   while(countedIterationsBefore<n){
-      iterationsOnCurrentSeed=0;
-      while(
-         nextVal<modulus &&
-         countedIterationsBefore + iterationsOnCurrentSeed < n
-      )
-      {
-         currVal = nextVal;
-         iterationsOnCurrentSeed++;
-         nextVal=currVal*p+q;
-      }
-      currVal=nextVal & modulus;
+   // this number is way too big, so code written in loop is not working
+   // somehow i should do this without actually performing the multiplication
+   // bit-array.cpp:31:10: warning: integer constant is so large that it is unsigned [enabled by default]
+   // cout<<14577145059443469734<<endl;
+
+   while(countedIterBefore<n){
+// cout<<"BIG "<<currVal<<endl;
+      iterOnCurrentSeed=0;
+      currVal=currVal % modulus; //starting see
+
       if (seeds.find(currVal) == seeds.end()){
-         countedIterationsBefore+=iterationsOnCurrentSeed;
+         countedIterBefore+=iterOnCurrentSeed;
          seeds.insert(currVal);
       }
+// cout<<"BIG2 "<<currVal<<endl;
+
+      while(currVal < modulus && countedIterBefore + iterOnCurrentSeed < n)
+      {
+// cout<<"s "<<currVal<<endl;
+         iterOnCurrentSeed++;
+         nextVal = currVal*p+q; //TODO: this gets out of bounds, even if i use large number multiplier, like this: https://gist.github.com/jargnar/3263916 then i have to trim last several digits, convert it to integer and perform modulus operation later
+         currVal = nextVal;
+// cout<<"s2 "<<currVal<<endl;
+// if (currVal > modulus) cout<<"XXXXXXXXXXXXXXXXXXXXXXXXX"<<endl;
+      }
    }
-   cout<<countedIterationsBefore<<endl;
+
+   cout<<countedIterBefore<<endl;
    return 0;
 }
