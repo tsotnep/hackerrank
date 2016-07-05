3e832d896788c7f4bfc8910444f7218a94fbf4f8
Tsotne Putkaradze <tsotnep@gmail.com>
2016-07-06 02:32:16 +0400

    simple commit

--- algorithms/strings/morgan-and-a-string.cpp
+++ algorithms/strings/morgan-and-a-string.cpp
@@ -6,22 +6,25 @@
 using namespace std;
 string a="ABBAAAA", b="AABBBAZZ", ans="";
 
-void fillup(int ia, int ib){
-    int da=a.size()-ia, db=b.size()-ib;
-    int d=da-db;
-    int c='Z'+1;    char cc=(char)c;    stringstream ss;    string ccc;    ss << cc;    ss >> ccc;
+inline void fillup(int ia, int ib, int sa, int sb){
+    // int da=sa-ia, db=sb-ib;
+    int d=(sa-ia)-(sb-ib);
+    // int c='Z'+1;    char cc=(char)c;    stringstream ss;    string ccc;    ss << cc;    ss >> ccc;
+    string ccc="[";
     while(d){
         if (d>0) {d--; b+=ccc;}
         else  if (d<0) {d++; a+=ccc;}
     }
 }
 
-int aISless(int ia, int ib){
-    int i=0;
-    while (i<a.size()){
+inline int aISless(int ia, int ib){
+    int i=0, sa=a.size()-ia;
+    while (i<sa){
         i++;
-        if(a[ia+i] < b[ib+i]) return 1;
-        if(a[ia+i] > b[ib+i]) return 0;
+        ia++;
+        ib++;
+        if(a[ia] < b[ib]) return 1;
+        if(a[ia] > b[ib]) return 0;
     }
     return 1;
 }
@@ -31,18 +34,16 @@ int main() {
 	int t = 1, sa,sb,ia,ib;
 	cin >> t;
 	while (t--) {
-		long long i = 0, j = 0;
-		cin >> a >> b;
+        cin >> a >> b;
         sa=a.size(); sb=b.size();
-        ia=0; ib=0;
-        ans = "";
+        ia=0; ib=0; ans = "";
 
         while(ia<sa && ib<sb){
             if (a[ia]<b[ib]) {ans+=a[ia]; ia++;}
             else{
                 if (a[ia]>b[ib]) {ans+=b[ib]; ib++;}
                 else{
-                    fillup(ia,ib);
+                    fillup(ia,ib,sa,sb);
                     // cout << a.substr(ia) << endl;
                     // cout << b.substr(ib) << endl<<endl;
                     if (aISless(ia,ib)) {ans+=a[ia]; ia++;}
@@ -50,8 +51,6 @@ int main() {
                 }
             }
         }
-
-		// cout << ans.substr(0,sa+sb) << endl;
         if (ia==sa) ans+=b.substr(ib,sb-ib);
         if (ib==sb) ans+=a.substr(ia,sa-ia);
 		cout << ans<< endl;
